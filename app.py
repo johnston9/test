3b5543ee6ed3796c17165679c1e3aa8dd43f1a88
johnston9 <davejohnston9@gmail.com>
2021-03-29 23:52:27 +0000

    test

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
